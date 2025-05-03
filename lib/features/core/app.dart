4ecbc7057796a25e6128da091a1ed7cec060c548
iqbalnova <iqbalnova707@gmail.com>
2025-05-03 15:58:21 +0700

    change-app-theme-by-system

--- lib/features/core/app.dart
+++ lib/features/core/app.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: !kDebugMode,
           theme: AppTheme.light(),
           darkTheme: AppTheme.dark(),
-          themeMode: ThemeMode.dark,
+          themeMode: ThemeMode.system,
           navigatorObservers: [routeObserver],
           onGenerateRoute: GetIt.instance<AppRouter>().onGenerateRoute,
         );
